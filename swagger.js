5265585cc07dd88936010f06e2ff173240496e59
shortytheman <shor7ty@gmail.com>
2025-02-20 20:30:50 +0100

    swagger ,.

--- swagger.js
+++ swagger.js
@@ -45,13 +45,39 @@ const options = {
             ]
           },
           description: 'Model to query'
+        },
+        modelId: {
+          in: 'path',
+          name: 'id',
+          required: true,
+          schema: {
+            oneOf: [
+              {
+                type: 'integer',
+                example: 1,
+                description: 'Numeric ID for MySQL database'
+              },
+              {
+                type: 'string',
+                pattern: '^[0-9a-fA-F]{24}$',
+                example: '507f1f77bcf86cd799439011',
+                description: 'MongoDB ObjectId'
+              }
+            ]
+          },
+          description: 'ID of the model (number for MySQL, ObjectId for MongoDB)'
         }
       },
       schemas: {
         Recipe: {
           type: 'object',
           properties: {
-            id: { type: 'number' },
+            id: { 
+              oneOf: [
+                { type: 'integer' },
+                { type: 'string' }
+              ]
+            },
             title: { type: 'string' },
             description: { type: 'string' },
             ingredients: { 
@@ -67,7 +93,12 @@ const options = {
         User: {
           type: 'object',
           properties: {
-            id: { type: 'number' },
+            id: { 
+              oneOf: [
+                { type: 'integer' },
+                { type: 'string' }
+              ]
+            },
             name: { type: 'string' },
             email: { type: 'string' }
           }
@@ -75,7 +106,12 @@ const options = {
         Ingredient: {
           type: 'object',
           properties: {
-            id: { type: 'number' },
+            id: { 
+              oneOf: [
+                { type: 'integer' },
+                { type: 'string' }
+              ]
+            },
             name: { type: 'string' },
             quantity: { type: 'string' }
           }
